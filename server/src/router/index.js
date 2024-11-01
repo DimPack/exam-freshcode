6269da5e0bd4072c16837eefb1d8bef31193c928
Dima Prosto <csourgg@gmail.com>
2024-11-01 20:10:37 +0200

    I moved the controller and router to separate files for registration and login and corrected the paths in restController.js

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
+const authRouter = require('./auth.router');
 const userRouter = require('./user.router');
 const contestRouter = require('./contest.router');
 const chatRouter = require('./chat.router');
 const { checkToken, checkAuth } = require('../middlewares/checkToken');
-const { validateLogin } = require('../middlewares/validators');
-const { login } = require('../controllers/userController');
 
 const router = express.Router();
-router.get('/getUser', checkAuth);
-router.post('/login', validateLogin, login);
+router.use('/', authRouter);
 
 router.use(checkToken);
 
-router.use('/', userRouter);
-router.use('/contest', contestRouter);
-router.use('/chat', chatRouter);
+router.get('/getUser', checkAuth);
+router.use('/users', userRouter);
+router.use('/contests', contestRouter);
+router.use('/chats', chatRouter);
 
 module.exports = router;
--- server/src/router/user.router.js
+++ server/src/router/user.router.js
@@ -4,28 +4,19 @@ const {
   updateUser,
   payment,
   changeMark,
-  registration,
-  login,
 } = require('../controllers/userController');
 const {
   onlyForCustomer,
   parseBody,
   onlyForCreative,
 } = require('../middlewares/basicMiddlewares');
-const {
-  validateRegistrationData,
-  validateLogin,
-  validateContestCreation,
-} = require('../middlewares/validators');
-const hashPass = require('../middlewares/hashPassMiddle');
+const { validateContestCreation } = require('../middlewares/validators');
 const { uploadContestFiles, uploadAvatar } = require('../utils/fileUpload');
 
 const router = Router();
 
-router.post('/registration', validateRegistrationData, hashPass, registration);
-// router.post('/login', validateLogin, login);
-/////^^^^^^
-
+router.post('/changeMark', onlyForCustomer, changeMark);
+router.post('/cashout', onlyForCreative, cashout);
 router.post(
   '/pay',
   onlyForCustomer,
@@ -34,10 +25,6 @@ router.post(
   validateContestCreation,
   payment
 );
-router.post('/changeMark', onlyForCustomer, changeMark);
-router.post('/cashout', onlyForCreative, cashout);
-
 router.put('/updateUser', uploadAvatar, updateUser);
-
 
 module.exports = router;
