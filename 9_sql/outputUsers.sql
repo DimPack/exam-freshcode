6ba7446d32690b0eef4c41e7239290b34d2bbee9
Dima Prosto <csourgg@gmail.com>
2024-11-17 00:29:38 +0200

    completed 9 tasks

--- 9_sql/outputUsers.sql
+++ 9_sql/outputUsers.sql
@@ -1,1 +1,7 @@
-SELECT * FROM "Users";
+SELECT json_object_agg(role, count) AS "roles"
+FROM (
+    SELECT role, COUNT(*) AS "count"
+    FROM "Users"
+    GROUP BY role
+    ORDER BY "count" DESC
+) AS "result";
