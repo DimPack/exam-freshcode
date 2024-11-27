9e08718e504a7c5b5631660d0c035935df8bd740
Dima Prosto <csourgg@gmail.com>
2024-11-27 23:49:28 +0200

    updated the task about error logs such as NODEJS_13, executed it taking into account additional logic

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -1,12 +1,81 @@
+const fs = require('fs');
+const path = require('path');
+
+const logFilePath = path.join(__dirname, 'error.log');
+const logDirPath = path.join(__dirname, 'Log');
+
+if (!fs.existsSync(logDirPath)) {
+  fs.mkdirSync(logDirPath);
+}
+
+const copyLogsDaily = () => {
+  const currentTimestamp = Date.now();
+  const targetFile = path.join(logDirPath, `${currentTimestamp}.log`);
+
+  fs.readFile(logFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Failed read log file:', err);
+      return;
+    }
+
+    const formattedData = data.split('\n').map(line => {
+      if (line.trim()) {
+        const logData = JSON.parse(line);
+        return JSON.stringify({
+          message: logData.message,
+          time: logData.time,
+          code: logData.code
+        });
+      }
+      return '';
+    }).join('\n');
+
+    fs.writeFile(targetFile, formattedData, 'utf8', (writeErr) => {
+      if (writeErr) {
+        console.error('Failed to write to target file:', writeErr);
+      } else {
+        console.log(`Error data written to ${targetFile}`);
+      }
+    });
+
+    fs.truncate(logFilePath, 0, (err) => {
+      if (err) {
+        console.error('Failed to clear log file:', err);
+      } else {
+        console.log('error.log has been cleared');
+      }
+    });
+  });
+};
+
+setInterval(copyLogsDaily, 86400000);
+
 module.exports = (err, req, res, next) => {
-  console.log(err);
-  if (err.message ===
-    'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
+  const logObject = {
+    message: err.message || 'Unknown Error',
+    time: Date.now(),
+    code: err.code || 500,
+    stackTrace: err.stack || 'No stack trace',
+  };
+
+  fs.appendFile(logFilePath, JSON.stringify(logObject) + '\n', (fileErr) => {
+    if (fileErr) {
+      console.error('Failed to write to log file:', fileErr);
+    } else {
+      console.log('Error logged in error.log');
+    }
+  });
+
+  if (
     err.message ===
-    'new row for relation "Users" violates check constraint "Users_balance_ck"') {
+      'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
+    err.message ===
+      'new row for relation "Users" violates check constraint "Users_balance_ck"'
+  ) {
     err.message = 'Not Enough money';
     err.code = 406;
   }
+
   if (!err.message || !err.code) {
     res.status(500).send('Server Error');
   } else {
