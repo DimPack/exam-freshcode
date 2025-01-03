4b690ca9d9663df65e542351757a380fc232f480
Dima <my_email@gmail.com>
2025-01-04 00:52:06 +0200

    complete work on the 5th task of TodyList

--- client/src/pages/ToDoEvents/ToDoEvents.jsx
+++ client/src/pages/ToDoEvents/ToDoEvents.jsx
@@ -1,15 +1,24 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import TodoForm from '../../components/TodoForm/TodoForm';
 import TodoList from '../../components/TodoList/TodoList';
 import styles from './ToDoEvents.module.sass';
 
-const ToDoEvents = () => {
+const ToDoEvents = ({ onCompletedEventsChange }) => {
   const [events, setEvents] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [deleteIndex, setDeleteIndex] = useState(null);
 
   const addEvent = (event) => {
-    setEvents([...events, event]);
+    const newEvent = { ...event, expired: false };
+    setEvents([...events, newEvent]);
+  };
+
+  const updateEventStatus = (eventIndex) => {
+    setEvents((prevEvents) => {
+      const updatedEvents = [...prevEvents];
+      updatedEvents[eventIndex].expired = true;
+      return updatedEvents;
+    });
   };
 
   const sortedEvents = useMemo(
@@ -32,6 +41,32 @@ const ToDoEvents = () => {
     closeModal();
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      setEvents((prevEvents) =>
+        prevEvents.map((event, index) => {
+          if (new Date(event.dateTime) <= now && !event.expired) {
+            updateEventStatus(index);
+          }
+          return event;
+        })
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [events]);
+
+  const completedEventsCount = useMemo(
+    () => events.filter((event) => event.expired).length,
+    [events]
+  );
+
+  useEffect(() => {
+    if (onCompletedEventsChange) {
+      onCompletedEventsChange(completedEventsCount);
+    }
+  }, [completedEventsCount, onCompletedEventsChange]);
+
   return (
     <div className={styles.mainTodo}>
       <TodoForm onAddEvent={addEvent} />
@@ -41,6 +76,7 @@ const ToDoEvents = () => {
         closeModal={closeModal}
         handleConfirmDelete={handleConfirmDelete}
         isModalOpen={isModalOpen}
+        updateEventStatus={updateEventStatus}
       />
     </div>
   );
