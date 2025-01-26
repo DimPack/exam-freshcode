2c4a53a179a56eff52ab4786f2d423d1669d18da
Dima <my_email@gmail.com>
2025-01-26 23:02:32 +0200

    create seeds moderator and started to fulfill

--- server/src/seeders/20241116224045-seed-moderators.js
+++ server/src/seeders/20241116224045-seed-moderators.js
@@ -1,29 +1,35 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const moderators = [];
     const currentDate = new Date();
 
     for (let i = 1; i <= 5; i++) {
+      const hashedPassword = await bcrypt.hash(`passwordModerator${i}`, 10);
+
       moderators.push({
         firstName: `Moderator${i}`,
         lastName: `Lastname${i}`,
         displayName: `moderator_${i}`,
-        password: 'hashed_password',
-        email: `moderator${i}@example.com`,
-        avatar: `https://example.com/avatar_moderator${i}.jpg`,
+        password: hashedPassword,
+        email: `moderator${i}@gmail.com`,
+        avatar: `anon.png`,
         role: 'moderator',
-        balance: Math.random() * 1000,
-        accessToken: `access_token_moderator_${i}`,
-        rating: (Math.random() * 5).toFixed(2),
+        balance: parseFloat((Math.random() * 1000).toFixed(2)),
+        accessToken: null,
+        rating: 0,
+        createdAt: currentDate,
+        updatedAt: currentDate,
       });
     }
 
     await queryInterface.bulkInsert('Users', moderators, {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', { role: 'moderator' }, {});
-  }
+  },
 };
