484f95cc0a9b8c9330b225409a46ad7683151fde
Dima <my_email@gmail.com>
2025-02-14 02:01:22 +0200

    update file moderatorPage

--- client/src/pages/ModeratorPage/ModeratorPage.jsx
+++ client/src/pages/ModeratorPage/ModeratorPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllOffers } from '../../store/slices/moderatorSlice';
-import style from './ModeratorPage.module.sass';
+import styles from './ModeratorPage.module.sass';
 import cx from 'classnames';
 
 const ModeratorPage = () => {
@@ -10,6 +10,7 @@ const ModeratorPage = () => {
   const [page, setPage] = useState(1);
   const [limit] = useState(10);
 
+  
   useEffect(() => {
     dispatch(fetchAllOffers({ page, limit }));
   }, [dispatch, page, limit]);
@@ -24,40 +25,43 @@ const ModeratorPage = () => {
     }
   };
 
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   const isNextPageAvailable = offers.length === limit;
 
-  const textContext = cx(style.creativeInfo, style.textContest);
+  const textContext = cx(styles.creativeInfo, styles.textContest);
   return (
     <div>
-      <div className={style.moderatorBlock}>
-        <h2 className={style.title}>List of all offers</h2>
+      <div className={styles.moderatorBlock}>
+        <h2 className={styles.title}>List of all offers</h2>
         {offers.length === 0 ? (
-          <p className={style.noOffers}>No offers available</p>
+          <p className={styles.noOffers}>No offers available</p>
         ) : (
-          <ul className={style.list}>
+          <ul className={styles.list}>
             {offers.map((offer) => (
-              <li key={offer.id} className={style.item}>
-                <div className={style.creativeInfo}>
-                  <p className={style.creativeName}>created an offer</p>
-                  <p className={style.titleCreative}>first name: <span className={style.infoCreative}>{offer.User.firstName}</span></p>
-                  <p className={style.titleCreative}>last name: <span className={style.infoCreative}>{offer.User.lastName}</span></p>
-                  <p className={style.titleCreative}>email: <span className={style.infoCreative}>{offer.User.email}</span></p>
+              
+              <li key={offer.id} className={styles.item}>
+                <div className={styles.creativeInfo}>
+                  <p className={styles.creativeName}>created an offer</p>
+                  <p className={styles.titleCreative}>first name: <span className={styles.infoCreative}> {offer.User.firstName}</span></p>
+                  <p className={styles.titleCreative}>last name: <span className={styles.infoCreative}>{offer.User.lastName}</span></p>
+                  <p className={styles.titleCreative}>email: <span className={styles.infoCreative}>{offer.User.email}</span></p>
                 </div>
                 <div className={textContext}>
-                  <p className={style.creativeName}>offer text</p>
+                  <p className={styles.creativeName}>offer text</p>
                   <p>{offer.text}</p>
                 </div>
-                <div className={style.creativeInfo}>
-                  <p className={style.creativeName}>status</p>
+                <div className={styles.creativeInfo}>
+                  <p className={styles.creativeName}>status</p>
                   <p>{offer.status}</p>
                 </div>
-                <div className={style.creativeInfo}>
-                  <p className={style.creativeName}>created a contest</p>
+                <div className={styles.creativeInfo}>
+                  <p className={styles.creativeName}>created a contest</p>
                   <p>{offer.Contest.User.email}</p>
                 </div>
+
               </li>
             ))}
           </ul>
@@ -67,7 +71,7 @@ const ModeratorPage = () => {
         <button onClick={handlePrevPage} disabled={page === 1}>
           Попередня сторінка
         </button>
-        <button onClick={handleNextPage} disabled={!isNextPageAvailable}>
+        <button onClick={()=> handleNextPage} disabled={!isNextPageAvailable}>
           Наступна сторінка
         </button>
       </div>
