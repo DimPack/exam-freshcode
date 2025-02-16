913b93d35465749f140615b2c8b143444fc9e028
Dima <my_email@gmail.com>
2025-02-17 00:34:46 +0200

    adjusted the path to status pictures

--- client/src/pages/ModeratorPage/statusIcons.js
+++ client/src/pages/ModeratorPage/statusIcons.js
@@ -1,6 +1,6 @@
-import pendingIcon from '../../../public/staticImages/icons/pending.svg';
-import wonIcon from '../../../public/staticImages/icons/done.svg';
-import rejectedIcon from '../../../public/staticImages/icons/close.svg';
+import pendingIcon from '/staticImages/icons/pending.svg';
+import wonIcon from '/staticImages/icons/done.svg';
+import rejectedIcon from '/staticImages/icons/close.svg';
 
 export const statusIcons = {
   pending: pendingIcon,
