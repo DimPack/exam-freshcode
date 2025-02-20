b402130aaa11bd8453ebfee231c90193b403c4e4
Dima <my_email@gmail.com>
2025-02-20 22:47:40 +0200

    rename file paginate

--- server/src/middlewares/paginate.mw.js
+++ server/src/middlewares/paginate.mw.js
@@ -1,13 +1,13 @@
 const constants = require('../constants');
 const { LIMIT_PAGINATION } = constants;
-module.exports.pagination = async (req, res, next) => {
+module.exports.paginate = async (req, res, next) => {
   try {
     const {
       query: { page, amount },
     } = req;
     const limit = amount > LIMIT_PAGINATION.MIN && amount <= LIMIT_PAGINATION.MAX ? amount : LIMIT_PAGINATION.MIN;
     const offset = page > 1 ? (page - 1) * limit : 0;
-    
+
     req.pagination = {
       limit,
       offset,
--- server/src/router/moderator.router.js
+++ server/src/router/moderator.router.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const { getAllOffers, updateOfferStatus } = require('../controllers/moderatorController');
-const { pagination } = require('../middlewares/pagination.mw');
+const { paginate } = require('../middlewares/paginate.mw');
 
 
 const router = Router();
 
 
-router.get('/getAllOffers', pagination, getAllOffers);
+router.get('/getAllOffers', paginate, getAllOffers);
 router.patch('/updateOfferStatus', updateOfferStatus);
 
 module.exports = router;
