a7800f99621072d5299286bd200b625a49c96bae
Dima <my_email@gmail.com>
2025-03-29 00:49:45 +0200

    changed passwords for working with the docker

--- server/src/seeders/20241116224045-seed-moderators.js
+++ server/src/seeders/20241116224045-seed-moderators.js
@@ -7,24 +7,21 @@ module.exports = {
     const moderators = [];
     const currentDate = new Date();
 
-    for (let i = 1; i <= 5; i++) {
-      const hashedPassword = await bcrypt.hash(`passwordModerator${i}`, 10);
-
-      moderators.push({
-        firstName: `Moderator${i}`,
-        lastName: `Lastname${i}`,
-        displayName: `moderator_${i}`,
-        password: hashedPassword,
-        email: `moderator${i}@gmail.com`,
-        avatar: `anon.png`,
-        role: 'moderator',
-        balance: parseFloat((Math.random() * 1000).toFixed(2)),
-        accessToken: null,
-        rating: 0,
-        createdAt: currentDate,
-        updatedAt: currentDate,
-      });
-    }
+    moderators.push({
+      firstName: `Moderator1`,
+      lastName: `Lastname1`,
+      displayName: `moderator_1`,
+      password: hashedPassword,
+      email: `moderator1@gmail.com`,
+      avatar: `anon.png`,
+      role: 'moderator',
+      balance: parseFloat((Math.random() * 1000).toFixed(2)),
+      accessToken: null,
+      rating: 0,
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    });
+    
 
     await queryInterface.bulkInsert('Users', moderators, {});
   },
